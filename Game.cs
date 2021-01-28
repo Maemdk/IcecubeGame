e9110ec31ab8f65d956f38878bbe02a3b54264dc
maemdk <maemdk@gmail.com>
2021-01-29 00:56:42 +0100

    Added tons of shit man :<

--- Game.cs
+++ Game.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -13,6 +17,23 @@ namespace TestGame
         private int _windowHeight;
         private int _score;
         private SpriteFont _font;
+        
+        private Texture2D _iceCubeTexture;
+        private int _iceCubeLastSpawnTime;
+        private Random _random = new Random();
+        private SoundEffect _iceCubeEscapeSound;
+        private SoundEffect _iceCubeEatSound;
+
+        private List<IceCube> _iceCubes = new List<IceCube>();
+
+        private int _maxSprintTime = 3;
+        private float _currentSprintTime;
+
+        public void SpawnIceCube(int positionX)
+        {
+            var iceCube = new IceCube(_iceCubeTexture, 4, new Vector2(positionX, -50), 50);
+            _iceCubes.Add(iceCube);
+        }
 
         public Game()
         {
@@ -32,7 +53,11 @@ namespace TestGame
                 new Vector2(_windowWidth / 2.0f - 50, _windowHeight - 100), 100);
 
             _font = Content.Load<SpriteFont>("font");
-        }
+            _iceCubeTexture = Content.Load<Texture2D>("images/icecube");
+            _iceCubeEscapeSound = Content.Load<SoundEffect>("sounds/iceyay");
+
+            _currentSprintTime = _maxSprintTime;
+;        }
 
         protected override void Update(GameTime gameTime)
         {
@@ -40,11 +65,62 @@ namespace TestGame
                 Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
+            PlayerMovement(gameTime);
+
+            if (_iceCubeLastSpawnTime + 1 < gameTime.TotalGameTime.TotalSeconds)
+            {
+                SpawnIceCube(_random.Next(0, _windowWidth - 50));
+                _iceCubeLastSpawnTime = (int) gameTime.TotalGameTime.TotalSeconds;
+            }
+
+            var aliveIceCubes = new List<IceCube>();
+            foreach (var iceCube in _iceCubes)
+            {
+                iceCube.Position = new Vector2(iceCube.Position.X, iceCube.Position.Y + iceCube.Speed);
+
+                if (iceCube.Position.Y >= _windowHeight)
+                {
+                    _score--;
+                    _iceCubeEscapeSound.Play();
+                } else if (_player.DestinationRectangle.Intersects(iceCube.DestinationRectangle))
+                {
+                    _score++;
+                }
+                else
+                {
+                    aliveIceCubes.Add(iceCube);
+                }
+            }
+
+            _iceCubes = aliveIceCubes;
+
+            base.Update(gameTime);
+        }
+
+        private void PlayerMovement(GameTime Gametime)
+        {
+            var currentSpeed = _player.Speed;
+
+            if (Keyboard.GetState().IsKeyDown(Keys.LeftShift))
+            {
+                if (_currentSprintTime > 0)
+                {
+                    currentSpeed *= 2;
+
+                    _currentSprintTime -= (float) Gametime.ElapsedGameTime.TotalSeconds;
+                }
+            }
+            else
+            {
+                _currentSprintTime += (float) Gametime.ElapsedGameTime.TotalSeconds;
+            }
+            _currentSprintTime = MathHelper.Clamp(_currentSprintTime, 0, 3);
+            
             if (Keyboard.GetState().IsKeyDown(Keys.D))
             {
                 if (_player.Position.X < _windowWidth - _player.Size)
                 {
-                    _player.Position = new Vector2(_player.Position.X + _player.Speed, _player.Position.Y);
+                    _player.Position = new Vector2(_player.Position.X + currentSpeed, _player.Position.Y);
                 }
             }
 
@@ -52,11 +128,9 @@ namespace TestGame
             {
                 if (_player.Position.X > 0)
                 {
-                    _player.Position = new Vector2(_player.Position.X - _player.Speed, _player.Position.Y);
+                    _player.Position = new Vector2(_player.Position.X - currentSpeed, _player.Position.Y);
                 }
             }
-
-            base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
@@ -66,6 +140,11 @@ namespace TestGame
             _spriteBatch.Begin();
             _spriteBatch.Draw(_player.Texture, _player.DestinationRectangle, Color.White);
             _spriteBatch.DrawString(_font, $"Score: {_score}", new Vector2(10, 10), Color.White);
+            _spriteBatch.DrawString(_font, $"Sprint: {Math.Ceiling(_currentSprintTime)}", new Vector2(10, 30), Color.White);
+            foreach (var iceCube in _iceCubes)
+            {
+                _spriteBatch.Draw(iceCube.Texture, iceCube.DestinationRectangle, Color.White);
+            }
             _spriteBatch.End();
 
             base.Draw(gameTime);
