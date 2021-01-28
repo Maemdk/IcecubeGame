30ffe75fcbefd9a2142c479240841fb47fa8c151
maemdk <maemdk@gmail.com>
2021-01-28 23:45:51 +0100

    First commit :)

---
+++ Game.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace TestGame
+{
+    public class Game : Microsoft.Xna.Framework.Game
+    {
+        private readonly GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private Player _player;
+        private int _windowWidth;
+        private int _windowHeight;
+
+        public Game()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void LoadContent()
+        {
+            _windowWidth = _graphics.PreferredBackBufferWidth;
+            _windowHeight = _graphics.PreferredBackBufferHeight;
+            
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            _player = new Player(Content.Load<Texture2D>("images/icedcoffee"), "John", 5,
+                new Vector2(_windowWidth / 2.0f - 50, _windowHeight - 100), 100);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
+                Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            if (Keyboard.GetState().IsKeyDown(Keys.D))
+            {
+                _player.Position = new Vector2(_player.Position.X + _player.Speed, _player.Position.Y);
+            }
+
+            if (Keyboard.GetState().IsKeyDown(Keys.A))
+            {
+                _player.Position = new Vector2(_player.Position.X - _player.Speed, _player.Position.Y);
+            }
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Red);
+
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(_player.Texture, _player.DestinationRectangle, Color.White);
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TestGame
+{
+    public class Player
+    {
+        public Texture2D Texture { get; set; }
+        public string Name { get; set; }
+        public float Speed { get; set; }
+        public Vector2 Position { get; set; }
+        public int Size { get; set; }
+        public Rectangle DestinationRectangle => new Rectangle((int) Position.X, (int) Position.Y, Size, Size);
+
+        public Player(Texture2D texture, string name, float speed, Vector2 position, int size)
+        {
+            Texture = texture;
+            Name = name;
+            Speed = speed;
+            Position = position;
+            Size = size;
+        }
+    }
+}
